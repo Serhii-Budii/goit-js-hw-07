dba123304a0813a56c07aa57a401b5140acd9b67
Serhii-Budii <seregaboodiy21@gmail.com>
2023-03-25 23:38:29 +0200

    hw7

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const addGalleryItems = galleryItems.map(({ preview, original, description }) => `<a class="gallery__item gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`).join("");
+galleryEl.insertAdjacentHTML('beforeend', addGalleryItems);
+
+let gallery = new SimpleLightbox('.gallery__item', {
+    captions: true,
+    captionDelay: 250,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+});
